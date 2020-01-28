547b1265ebacf14219e23ab09545a3fe531ad778
David Levente Dozsa <dozsalevi@gmail.com>
2020-01-28 06:45:29 +0000

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-spring-cards
-Created with CodeSandbox
---
+++ build/precache-manifest.1e8bfa22020cab383ab273593e6d4a22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a296bab11c6e7d31d673bf3b47a963",
+    "url": "/s/new/index.html"
+  },
+  {
+    "revision": "6d4f781d80836535210e",
+    "url": "/s/new/static/css/main.b7deaf74.chunk.css"
+  },
+  {
+    "revision": "8bb3031fe6eee1c20127",
+    "url": "/s/new/static/js/2.f4b647c9.chunk.js"
+  },
+  {
+    "revision": "6d4f781d80836535210e",
+    "url": "/s/new/static/js/main.1ed4ef9a.chunk.js"
+  },
+  {
+    "revision": "85945ddbca995500257a",
+    "url": "/s/new/static/js/runtime~main.01695a31.js"
+  }
+]);
